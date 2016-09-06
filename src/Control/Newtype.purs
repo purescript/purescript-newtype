1b0854ade942f6d81db5c095f8898c7dfde22ee2
Gary Burgess <gary.burgess@gmail.com>
2016-09-06 15:08:57 +0100

    Initial commit

---
+++ src/Control/Newtype.purs
@@ -0,0 +1,62 @@
+module Control.Newtype where
+
+import Prelude
+
+class Newtype t a | t -> a where
+  wrap :: a -> t
+  unwrap :: t -> a
+
+op :: forall t a. Newtype t a => (a -> t) -> t -> a
+op _ = unwrap
+
+ala
+  :: forall f t a s b
+   . (Functor f, Newtype t a, Newtype s b)
+  => (a -> t)
+  -> ((b -> s) -> f t)
+  -> f a
+ala _ f = map unwrap (f wrap)
+
+alaF
+  :: forall f g t a s b
+   . (Functor f, Functor g, Newtype t a, Newtype s b)
+  => (a -> t)
+  -> (f t -> g s)
+  -> f a
+  -> g b
+alaF _ f = map unwrap <<< f <<< map wrap
+
+under
+  :: forall t a s b
+   . (Newtype t a, Newtype s b)
+  => (a -> t)
+  -> (t -> s)
+  -> (a -> b)
+under _ f = unwrap <<< f <<< wrap
+
+over
+  :: forall t a s b
+   . (Newtype t a, Newtype s b)
+  => (a -> t)
+  -> (a -> b)
+  -> t
+  -> s
+over _ f = wrap <<< f <<< unwrap
+
+underF
+  :: forall f t a s b
+   . (Newtype t a, Newtype s b, Functor f)
+  => (a -> t)
+  -> (f t -> f s)
+  -> f a
+  -> f b
+underF _ f = map unwrap <<< f <<< map wrap
+
+overF
+  :: forall f t a s b
+   . (Newtype t a, Newtype s b, Functor f)
+  => (a -> t)
+  -> (f a -> f b)
+  -> f t
+  -> f s
+overF _ f = map wrap <<< f <<< map unwrap
